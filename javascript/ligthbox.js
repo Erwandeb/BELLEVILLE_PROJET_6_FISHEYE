44e97b36abc55074ffd5bc4da8baefa3e6be21b3
Erwan de Belleville <edebelleville@hotmail.fr>
2021-02-12 10:47:06 +0100

    initial repository

--- javascript/ligthbox.js
+++ javascript/ligthbox.js
@@ -1,64 +1,76 @@
 
+/*--------------------------------------------OUVERTURE  DE LA LIGHTBOX-------------------------------------------*/
 
-const lightBox = document.querySelector('.lightbox');
-const slider = document.querySelector('.slider')
-
-/*
 // Ouverture de la lightbox 
-const figure = document.getElementsByTagName(".carroussel img");
+const lightBox = document.querySelector('.lightbox');
+const carrousselImage = document.querySelectorAll('.carroussel-img');
 
-figure.addEventListener('click', openLightBox);
-function openLightBox(){
-    lightBox.style.display ="block";
+for(let i=0 ; i<carrousselImage.length; i++){
+    carrousselImage[i].addEventListener('click', openBox);
+}
+
+function openBox(){
+    lightBox.style.opacity ="1";
+    lightBox.style.zIndex="10";
 };
+
 
 // Fermeture de la lightBox 
 const closeLightBox = document.querySelector('.closeLightbox');
 
 closeLightBox.addEventListener('click', closebox)
 function closebox(){
-    lightBox.style.display ="none";
+    lightBox.style.opacity ="0";
+    lightBox.style.zIndex="-10";
 };
-*/
 
-// Défilement de la lightbox 
+/*--------------------------------------FONCTIONNELENT DE LA LIGHTBOX-------------------------------------------*/
+
+// Variables et constantes
 const suivantLightBox = document.querySelector('.suivantLightBox');
 const precedentLightBox = document.querySelector('.precedentLightBox');
-//const lightBoxImageView = docment.getElementsByClassName('lightBox-img-view');
 const lightBoxImage = document.querySelectorAll('.lightBox-img');
 const lastClone = document.getElementById ('lastClone');
 const firstClone = document.getElementById ('firstClone');
+const slider = document.querySelector('.slider')
 
-// Counter 
+// Compteur
 let counter = 1;
 const size = lightBoxImage[0].clientWidth;
 
-slider.style.transform = 'translateX(' +(-size*counter) +'px)';
+slider.style.transform = 'translateX(' + (-size*counter) +'px)';
 
 
-
-// Bouton listener 
+// Bouton SUIVANT
 suivantLightBox.addEventListener('click', moveRight);
 function moveRight(){
+    if(counter >= lightBoxImage.length - 1) return ;
     counter ++;
-    slider.style.transition = ' transform 0.2s ease-in-out';
-    slider.style.transform = 'translateX(' +(-size*counter) +'px)';
+    slider.style.transition = ' transform 0.250s ease-in-out';
+    slider.style.transform = 'translateX(' + (-size*counter) +'px)';
 };
 
-
+// Bouton PRECEDENT
  precedentLightBox.addEventListener('click', moveLeft);
  function moveLeft(){
+     if(counter<=0) return;
     counter --;
-    slider.style.transition = ' transform 0.2s ease-in-out';
-    slider.style.transform = 'translateX(' +(-size*counter) +'px)';
+    slider.style.transition = ' transform 0.250s ease-in-out';
+    slider.style.transform = 'translateX(' + (-size*counter) +'px)';
 };
 
-slider.addEventListener('transitionend', loopDroit);
 
-function loopDroit(){
-    if (lightBoxImage[counter].id === lastClone){
+// Défilement des photos
+slider.addEventListener('transitionend', loop);
+function loop(){
+    if (lightBoxImage[counter].id === 'lastClone'){
         slider.style.transition = "none";
         counter = lightBoxImage.length - 2;
-        slider.style.transform = 'translateX(' +(-size*counter) +'px)';
+        slider.style.transform = 'translateX(' + (-size*counter) +'px)';
+    }
+    if(lightBoxImage[counter].id === 'firstClone'){
+        slider.style.transition = "none";
+        counter = lightBoxImage.length - counter;
+        slider.style.transform = 'translateX(' + (-size*counter) +'px)';
     }
 }
---
+++ javascript/modale.js
@@ -0,0 +1,128 @@
+/* ------------------------Ouverture du formulaire ----------------------------*/
+
+const contactMe = document.querySelector('.contact');
+const modal = document.querySelector('.fenetre-modale');
+const closeBtn = document.querySelector('.closeBtn');
+
+
+// Lancement de la modale
+contactMe.addEventListener('click', lauchModal);
+
+function lauchModal(){
+    modal.style.display = "block";
+}
+
+//fermeture de la modale
+
+closeBtn.addEventListener('click', closeModal);
+
+function closeModal(){
+    modal.style.display ="none";
+}
+
+
+/* ------------------------Traitement du formulaire ----------------------------*/
+
+// Variables
+const firstName = document.getElementById('firstname');
+const lastName = document.getElementById('lastname');
+const eMail = document.getElementById('email');
+const messageTexte = document.getElementById('message');
+const form = document.getElementById('form');
+let firstnameRegExp = new RegExp ('[0-9]');
+let lastnameRegExp = new RegExp ('[0-9]');
+const fenetreConfirmation = document.querySelector('.fenetre-validation-formulaire');
+
+let isFirstNameValid = false;
+let isLastNameValid = false;
+let isEmailValid = false;
+let isMessageTextValid = false
+
+
+const errorFirstName = document.querySelector('.errorFirstName');
+const errorLastName =document.querySelector('.errorlastName');
+const erroreMail = document.querySelector('.erroreMail');
+const erroreMessageText = document.querySelector('.erroreMessageText');
+
+
+
+form.addEventListener('submit', validate)
+
+
+
+function validate(e){
+e.preventDefault();
+
+// Vérification du champ PRENOM
+if(firstName.value.trim() == ""){
+    firstName.style.border = "2px solid #901C1C";
+    errorFirstName.innerHTML = "Vous devez écrire votre prénom.";
+    isFirstNameValid = false;
+  } else if (firstnameRegExp.test(firstName.value)){
+    errorFirstName.innerHTML = "Le champ prénom ne doit pas comporter de chiffres.";
+    firstName.style.border = "2px solid #901C1C";
+    isFirstNameValid = false;
+  }
+  else {
+    isFirstNameValid = true;
+    firstName.style.border = "";
+    errorFirstName.innerHTML = "";
+  }
+
+  // Vérification du champ NOM
+  if(lastName.value.trim() === ""){
+    lastName.style.border = "2px solid #901C1C";
+    errorLastName.innerHTML = "Vous devez écrire votre nom.";
+    isLastNameValid = false;
+  } else if (lastnameRegExp.test(lastName.value)){
+    errorLastName.innerHTML = "Le champ nom ne doit pas comporter de chiffres.";
+    lastName.style.border = "2px solid #901C1C";
+    isLastNameValid = false;
+  }
+  else {
+    isLastNameValid = true;
+    lastName.style.border = "";
+    errorLastName.innerHTML = "";
+  }
+
+   // Vérification du champ EMAIL
+   if(eMail.value.trim() === ""){
+    eMail.style.border = "2px solid #901C1C";
+    erroreMail.innerHTML = "Vous devez renseigner votre e-mail.";
+    isEmailValid = false;
+  } else {
+    isEmailValid = true;
+    eMail.style.border = "";
+    erroreMail.innerHTML = "";
+  }
+
+   // Vérification du champ MESSAGE TEXTE
+   if(messageTexte.value.trim() === ""){
+    messageTexte.style.border = "2px solid #901C1C";
+    erroreMessageText.innerHTML = "Vous devez écrire votre message.";
+    isMessageTextValid = false;
+  } else {
+    isMessageTextValid = true;
+    messageTexte.style.border = "";
+    erroreMessageText.innerHTML = "";
+  }
+
+  if(  isFirstNameValid && isLastNameValid &&  isEmailValid && isMessageTextValid){
+
+    closeModal();
+    removeData();
+    openFenetreConfirmation()
+
+  }
+};
+
+function removeData(){
+    firstName.value ="";
+    lastName.value="";
+    eMail.value="";
+    messageTexte.value ="";
+}
+
+function openFenetreConfirmation(){
+    fenetreConfirmation.style.display ="block";
+}
