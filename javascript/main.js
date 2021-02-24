29e5ab7a727f0f819a66c221e019d11f42e9141f
Erwan de Belleville <edebelleville@hotmail.fr>
2021-02-24 08:32:49 +0100

    modifs CRUD

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,28 @@
-const postList = document.querySelector('.post-list');
-let output ="";
+const mainDiv = document.getElementById('main');
+
+fetch('javascript/data.json')
+  .then((response) => response.json())
+  .then(function (data) {
+    for (photographer of data.photographers) {
+      mainDiv.innerHTML += `
+        <article>
+                <a href="mimiKeel.html"><img src="photos/Photographers-ID-Photos/${photographer.portrait}"
+                        alt=" un potrait de Nora avec des lunettes" /></a>
+                <div class="description-article">
+                    <h2>${photographer.name}</h2>
+                    <h3>London, UK</h3>
+                    <blockquote lang="en-GB">Voir le beau dans le quotidien</blockquote>
+                    <p>400€/jour</p>
+                </div>
+                <div class="filtres-articles">
+                    <button type="button">#Portrait</button>
+                    <button type="button">#Events</button>
+                    <button type="button">#Travel</button>
+                    <button type="button">#Animals</button>
+                </div>
+            </article>
+      `;
+    }
+  });
 
 
