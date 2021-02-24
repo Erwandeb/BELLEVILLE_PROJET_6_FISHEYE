6ce566abe261b42b5783bd3d6b8e040ecc5caa2f
Erwan de Belleville <edebelleville@hotmail.fr>
2021-02-24 09:44:57 +0100

    ajouts

--- javascript/main.js
+++ javascript/main.js
@@ -1,27 +1,32 @@
+
+// Creation d'une variable pour insérer les blocs HTML
 const mainDiv = document.getElementById('main');
 
+// Récupération des données JSON
 fetch('javascript/data.json')
   .then((response) => response.json())
   .then(function (data) {
+   
+// Boucle qui parcours le fichier photographe dans le JSON
     for (photographer of data.photographers) {
-      mainDiv.innerHTML += `
+      
+// Création d'un bloc par photographe
+        mainDiv.innerHTML += `
         <article>
                 <a href="mimiKeel.html"><img src="photos/Photographers-ID-Photos/${photographer.portrait}"
-                        alt=" un potrait de Nora avec des lunettes" /></a>
+                        alt="${photographer.description}"/></a>
                 <div class="description-article">
                     <h2>${photographer.name}</h2>
-                    <h3>London, UK</h3>
-                    <blockquote lang="en-GB">Voir le beau dans le quotidien</blockquote>
-                    <p>400€/jour</p>
-                </div>
-                <div class="filtres-articles">
-                    <button type="button">#Portrait</button>
-                    <button type="button">#Events</button>
-                    <button type="button">#Travel</button>
-                    <button type="button">#Animals</button>
+                    <h3>${photographer.city}, ${photographer.country}</h3>
+                    <blockquote>${photographer.tagline}</blockquote>
+                    <p>${photographer.price}€/jour</p>
                 </div>
             </article>
-      `;
+            <div class="filtres-articles">
+                    <button type="button">#${photographer.tags[i]}</button>
+            </div>
+     `;
+   
     }
   });
 
